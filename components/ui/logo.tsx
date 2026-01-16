1ab0f6470ad38cf76964e3277dbae2c131c738be
Diogo Ferreira <diogoferreira229@gmail.com>
2026-01-16 00:22:16 +0000

    update logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href="/" className="inline-flex" aria-label="Hackstle">
       <Image
-        src="/images/hackstle-logo1.png"
+        src="/images/hackstle-logo.png"
         alt="Hackstle logo"
         width={370}
         height={114}
